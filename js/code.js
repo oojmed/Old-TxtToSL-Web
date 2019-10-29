1ffb3619e9bb94806ed64a64d27976a7c54de4ea
Oj18 <oj@oojmed.com>
2019-10-29 15:41:04 +0000

    Move JS code / files to /js/

--- js/code.js
+++ js/code.js
@@ -170,7 +170,7 @@ async function registerSW() {
 
   if ('serviceWorker' in navigator) {
     try {
-      navigator.serviceWorker.register('./sw.js').then(reg => {
+      navigator.serviceWorker.register('/js/code.js').then(reg => {
         reg.addEventListener('updatefound', () => {
           newWorker = reg.installing;
 
