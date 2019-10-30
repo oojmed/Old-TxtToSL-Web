b8070c07fd44fa436d4efa87e1b03d8f4462ef09
Oj18 <oj@oojmed.com>
2019-10-30 21:30:59 +0000

    Add Halloween styling

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,5 @@
 const baseCacheName = 'TxtToSL-Web';
-const versionName = 'v2019-10-29.9';
+const versionName = 'v2019-10-30.1';
 const cacheName = baseCacheName + '-' + versionName;
 const runtimeCacheName = cacheName + '-runtime';
 
