e8272dbdf9fc290449066321095d79b44e36f562
Oj18 <oj@oojmed.com>
2019-12-15 15:53:11 +0000

    Added Gfycat as a new hoster option

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const baseCacheName = 'TxtToSL-Web';
-const versionName = 'v2019-12-15.2';
+const versionName = 'v2019-12-15.3';
 const cacheName = baseCacheName + '-' + versionName;
 const runtimeCacheName = cacheName + '-runtime';
 
