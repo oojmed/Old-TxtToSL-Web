aed0455fd1caccf00b5d47a79c597f30af801379
Oj <oj@oojmed.com>
2020-03-28 11:59:12 +0000

    Added noopener rel to external links

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const baseCacheName = 'TxtToSL-Web';
-const versionName = 'v2019-12-22.1';
+const versionName = 'v2020-03-28.1';
 const cacheName = baseCacheName + '-' + versionName;
 const runtimeCacheName = cacheName + '-runtime';
 
